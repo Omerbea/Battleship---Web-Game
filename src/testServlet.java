13fda6c3d3fc8d5a8d40e9af42dd209ede94e8b8
Jonathan Benedek <jonathan.benedek@mce-sys.com>
2017-10-01 19:24:50 +0300

    init

--- src/testServlet.java
+++ src/testServlet.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.util.*;
 
 @WebServlet (name = "testServlet" , urlPatterns = {"/test"})
-
 public class testServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
